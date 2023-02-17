7fb7a09c78338531ba8bb0900619d40186c706ac
Aliaxander Novikov <nerevarin11@gmail.com>
2023-02-17 14:57:37 +0300

    feat: add handlebars and materialize

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const app = express();
 
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
